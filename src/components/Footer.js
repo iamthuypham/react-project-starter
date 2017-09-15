ccea6b070707d79542c46b7ded85628f0e9a47e4
Ian Wright <ian@authentise.com>
2017-09-14 19:24:59 -0600

    Create Footer component and styles

---
+++ src/components/Footer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Button from 'material-ui/Button';
+
+require('./Footer.scss');
+
+const Footer = () => (
+  <footer>
+    <div className="bg-grey flex-even padded-vertical">
+
+      <div>
+        <strong className="block padded-below">
+          Connect with AsylumConnect
+        </strong>
+        <div className="flex-even">
+          <div className="circle" />
+          <div className="circle" />
+          <div className="circle" />
+          <div className="circle" />
+          <div className="circle" />
+          <div className="circle" />
+        </div>
+      </div>
+
+      <div>
+        <strong className="block padded-below">
+          Stay Updated on AsylumConnect's Lifesaving Initiatives
+        </strong>
+        <div className="bg-white border-blue rounded">
+          <Button className=" bg-white blue-border rounded">
+            <a href="http://www.asylumconnect.org/follow-us-subscribe/">
+              <strong className="text-blue">
+                Subscribe to our newsletter
+              </strong>
+            </a>
+          </Button>
+        </div>
+      </div>
+
+    </div>
+    <div className="bg-light-grey padded-vertical">
+      AsylumConnect, Inc. 2016. All rights reserved. Powered by One Degree.
+    </div>
+  </footer>
+);
+
+export default Footer;
