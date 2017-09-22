0770d258fc8d534ebf76ba23f15fbf839015a4f9
Thuy <tpham045@hotmail.com>
2017-09-22 19:04:11 +0000

    Change theme color, typography and JSS for button

--- src/components/AsylumConnectCatalog.js
+++ src/components/AsylumConnectCatalog.js
@@ -1,51 +1,83 @@
 import React from 'react';
-import Dialog, { DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle } from 'material-ui/Dialog';
+import Typography from 'material-ui/Typography'
 import Button from 'material-ui/Button';
+import TextField from 'material-ui/TextField'
+import AppBar from 'material-ui/AppBar'
+import { withStyles } from 'material-ui/styles';
+import PropTypes from 'prop-types'
+import classNames from 'classnames';
+
 require('./AsylumConnectCatalog.scss');
+
+// 1. We define the styles.
+const styles = theme => ({
+  button: {
+    color: theme.palette.primary[500],
+    border: '1px solid #6a88c0',
+    'border-radius': '50px',
+    '&:hover': {
+      color: 'white',
+      backgroundColor: '#6a88c0',
+    },
+  }
+});
+
+function MyLink(props) {
+  const { children, classes, className, variant} = props;
+
+  return (
+    <Button className={classNames(
+        classes.button,
+        
+        className,
+      )}
+    >
+      {children}
+    </Button>
+  );
+}
+
+MyLink.propTypes = {
+  children: PropTypes.node.isRequired,
+  classes: PropTypes.object.isRequired,
+  className: PropTypes.string,
+  variant: PropTypes.oneOf(['primary']),
+};
+
+// 2. We inject the styles.
+const MyLinkStyled = withStyles(styles)(MyLink);
 
 export default class AsylumConnectCatalog extends React.Component { 
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      open: false,
-    };
-    this.handleRequestClose = this.handleRequestClose.bind(this);
   }
-
-  handleRequestClose() {
-    this.setState({
-      open: false,
-    });
-  }
-
   render() {
     return (
       <div>
         <header>
           <nav/>
         </header>
-        <div className="content" >
-          <Button raised color="primary" onClick={() => this.setState({ open: true })}>Open alert dialog</Button>
-          <Dialog open={this.state.open} onRequestClose={this.handleRequestClose}>
-            <DialogTitle>Dialog With Actions</DialogTitle>
-            <DialogContent>
-              <DialogContentText>
-                The actions in this window were passed in as an array of React objects.
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={this.handleRequestClose} color="primary">
-                Disagree
-              </Button>
-              <Button onClick={this.handleRequestClose} color="primary">
-                Agree
-              </Button>
-            </DialogActions>
-          </Dialog>
+        <div className='content' >
+          <Typography type='display1'>Heading 1 [display1]</Typography>
+          <Typography type='display2'>Heading 2 [display2]</Typography>
+          <Typography type='display3'>Heading 3 [display3]</Typography>
+          <Typography type='display4'>Nav [display4]</Typography>
+          <Typography type='body1'>Paragraph [body1]</Typography>
+          <Typography type='body2'>Hint Text [body2]</Typography>
+          <Button className="${classes.button}">Open</Button>
+          <form noValidate autoComplete="off">
+            <TextField
+              label="With placeholder"
+              placeholder="Placeholder"
+              margin="normal"
+            />
+          </form>
+          
+          <MyLinkStyled href="#">{'MyLink'}</MyLinkStyled>
+          {' - '}
+          <MyLinkStyled href="#" variant="primary">
+            {'primary'}
+          </MyLinkStyled>
         </div>
         <footer />
       </div>
